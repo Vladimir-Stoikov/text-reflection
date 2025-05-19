0726af46b1fc055a346712126ee6b1fd0bf297eb
Vladimir Stoikov <vspsmoke@gmail.com>
2025-05-19 12:04:25 +0300

    add textmanager

---
+++ src/textManager.js
@@ -0,0 +1,56 @@
+import { FontLoader } from 'three/addons/loaders/FontLoader.js';
+import { TextGeometry } from 'three/addons/geometries/TextGeometry.js';
+
+export class TextManager {
+  constructor(scene) {
+    this.scene = scene;
+    this.group = new THREE.Group();
+    this.group.position.y = 100;
+    this.scene.add(this.group);
+
+    this.font = null;
+    this.text = 'three.js';
+    this.loadFont('optimer', 'bold');
+  }
+
+  loadFont(fontName, fontWeight) {
+    const loader = new FontLoader();
+    loader.load(`fonts/${fontName}_${fontWeight}.typeface.json`, response => {
+      this.font = response;
+      this.createText();
+    });
+  }
+
+  createText() {
+    while (this.group.lenght > 0) {
+      this.group.remove(this.group.children[0]);
+    }
+
+    if (!this.font || !this.text) return;
+
+    const textGeo = new TextGeometry(this.text, {
+      font: this.font,
+      size: 70,
+      height: 20,
+      curveSegments: 4,
+      bevelThickness: 2,
+      bevelSize: 1.5,
+      bevelEnabled: true,
+    });
+
+    textGeo.computerBoundingBox();
+    const centerOffset = -0.5 * (textGeo.computerBoundingBox.max.x - textGeo.computerBoundingBox.min.x);
+    const material = new THREE.MeshPhongMaterial({ color: 0xffffff, flatShading: true });
+    const textMesh = new THREE.Mesh(textGeo, material);
+
+    textMesh.position.x = centerOffset;
+    textMesh.position.y = 30;
+
+    this.group.add(textMesh);
+  }
+
+  updateText(newText) {
+    this.text = newText;
+    this.createText();
+  }
+}
