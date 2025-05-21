254f8d6630659e8ae4844a4fa07f34da870323fc
Vladimir Stoikov <vspsmoke@gmail.com>
2025-05-21 11:18:14 +0300

    add mirrored text

--- src/textManager.js
+++ src/textManager.js
@@ -11,7 +11,18 @@ export class TextManager {
 
     this.font = null;
     this.text = 'three.js';
+    this.mirror = true;
+    this.hover = 35;
+    this.depth = 50;
     this.loadFont('optimer', 'bold');
+
+    // Add plane
+    const planeGeo = new THREE.PlaneGeometry(10000, 10000);
+    const planeMat = new THREE.MeshBasicMaterial({ color: 0xffffff, opacity: 0.5, transparent: true });
+    const plane = new THREE.Mesh(planeGeo, planeMat);
+    plane.position.y = 100;
+    plane.rotation.x = -Math.PI / 2;
+    this.scene.add(plane);
   }
 
   loadFont(fontName, fontWeight) {
@@ -41,13 +52,31 @@ export class TextManager {
 
     textGeo.computeBoundingBox();
     const centerOffset = -0.5 * (textGeo.boundingBox.max.x - textGeo.boundingBox.min.x);
-    const material = new THREE.MeshPhongMaterial({ color: 0xffffff, flatShading: true });
-    const textMesh = new THREE.Mesh(textGeo, material);
 
-    textMesh.position.x = centerOffset;
-    textMesh.position.y = 30;
+    const materials = [
+      new THREE.MeshPhongMaterial({ color: 0xffffff, flatShading: true }), // front
+      new THREE.MeshPhongMaterial({ color: 0xffffff }), // side
+    ];
 
-    this.group.add(textMesh);
+    // Main text
+    const textMesh1 = new THREE.Mesh(textGeo, materials);
+    textMesh1.position.x = centerOffset;
+    textMesh1.position.y = this.hover;
+    textMesh1.position.z = 0;
+    textMesh1.rotation.x = 0;
+    textMesh1.rotation.y = Math.PI * 2;
+    this.group.add(textMesh1);
+
+    // Mirror text
+    if (this.mirror) {
+      const textMesh2 = new THREE.Mesh(textGeo, materials);
+      textMesh2.position.x = centerOffset;
+      textMesh2.position.y = -this.hover;
+      textMesh2.position.z = this.depth;
+      textMesh2.rotation.x = Math.PI;
+      textMesh2.rotation.y = Math.PI * 2;
+      this.group.add(textMesh2);
+    }
   }
 
   updateText(newText) {
